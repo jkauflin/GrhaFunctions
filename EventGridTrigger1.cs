697d59a55ce5d6372bfc538b0675f9df3499e5bd
John J Kauflin <johnkauflin@gmail.com>
2025-08-29 07:22:30 -0400

    Initial create

---
+++ EventGridTrigger1.cs
@@ -0,0 +1,25 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+
+using System;
+using Azure.Messaging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace GrhaWeb.Function;
+
+public class EventGridTrigger1
+{
+    private readonly ILogger<EventGridTrigger1> _logger;
+
+    public EventGridTrigger1(ILogger<EventGridTrigger1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(EventGridTrigger1))]
+    public void Run([EventGridTrigger] CloudEvent cloudEvent)
+    {
+        _logger.LogInformation("Event type: {type}, Event subject: {subject}", cloudEvent.Type, cloudEvent.Subject);
+    }
+}
